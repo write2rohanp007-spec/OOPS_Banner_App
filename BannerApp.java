37155fa184a59da4fccd9d41cbe9d22a94c6b9d1
write2rohanp007-spec <write2rohanp007@gmail.com>
2026-03-09 20:05:09 +0530

    UC6: Refactor banner logic using helper methods

--- BannerApp.java
+++ BannerApp.java
@@ -2,18 +2,61 @@ public class BannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        banner[0] = String.join(" ", " ***** ", " ***** ", " ****** ", " ***** ");
-        banner[1] = String.join(" ", "*     *", "*     *", "*     * ", "*     *");
-        banner[2] = String.join(" ", "*     *", "*     *", "*     * ", "*     ");
-        banner[3] = String.join(" ", "*     *", "*     *", "****** ", " ***** ");
-        banner[4] = String.join(" ", "*     *", "*     *", "*      ", "     *");
-        banner[5] = String.join(" ", "*     *", "*     *", "*      ", "*    *");
-        banner[6] = String.join(" ", " ***** ", " ***** ", "*      ", " **** ");
+        String[] banner = {
+            String.join(" ", o[0], o[0], p[0], s[0]),
+            String.join(" ", o[1], o[1], p[1], s[1]),
+            String.join(" ", o[2], o[2], p[2], s[2]),
+            String.join(" ", o[3], o[3], p[3], s[3]),
+            String.join(" ", o[4], o[4], p[4], s[4]),
+            String.join(" ", o[5], o[5], p[5], s[5]),
+            String.join(" ", o[6], o[6], p[6], s[6])
+        };
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Method to create pattern for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Method to create pattern for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Method to create pattern for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*      ",
+            " ***** ",
+            "      *",
+            "*     *",
+            " ***** "
+        };
     }
 }
