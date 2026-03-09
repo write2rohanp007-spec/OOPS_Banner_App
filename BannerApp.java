3d60360619dd9e09c46a61ecbff3992a5f7b8ad1
write2rohanp007-spec <write2rohanp007@gmail.com>
2026-03-09 17:54:27 +0530

    UC3: Render OOPS banner using String.join()

---
+++ BannerApp.java
@@ -0,0 +1,26 @@
+public class BannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  *****  ", "  *****  ", " ******  ", " *****  "));
+
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " *     * ", "*     * "));
+
+        System.out.println(String.join(" ",
+                "*       *", "*       *", " *     * ", "*      "));
+
+        System.out.println(String.join(" ",
+                "*       *", "*       *", " ******  ", " ***** "));
+
+        System.out.println(String.join(" ",
+                "*       *", "*       *", " *       ", "      *"));
+
+        System.out.println(String.join(" ",
+                " *     * ", " *     * ", " *       ", "*     *"));
+
+        System.out.println(String.join(" ",
+                "  *****  ", "  *****  ", " *       ", " ***** "));
+    }
+}
