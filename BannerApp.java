0aa33280c1428048f536ecf827e2ee379c4b4cc3
write2rohanp007-spec <write2rohanp007@gmail.com>
2026-03-09 20:12:34 +0530

    UC7: Store character pattern in CharacterPattern class

--- BannerApp.java
+++ BannerApp.java
@@ -1,19 +1,81 @@
 public class BannerApp {
 
+    // Inner Static Class to store character and its pattern
+    static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        // Constructor
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        // Getter for character
+        public char getCharacter() {
+            return character;
+        }
+
+        // Getter for pattern
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    // Utility method to build one line of the banner
+    public static String buildLine(CharacterPatternMap[] letters, int row) {
+
+        StringBuilder line = new StringBuilder();
+
+        for (CharacterPatternMap letter : letters) {
+            line.append(letter.getPattern()[row]).append(" ");
+        }
+
+        return line.toString();
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
+        // Pattern for letter O
+        CharacterPatternMap O = new CharacterPatternMap('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
 
-        banner[0] = String.join(" ", " ***** ", " ***** ", " ****** ", " ***** ");
-        banner[1] = String.join(" ", "*     *", "*     *", "*     * ", "*     *");
-        banner[2] = String.join(" ", "*     *", "*     *", "*     * ", "*     ");
-        banner[3] = String.join(" ", "*     *", "*     *", "****** ", " ***** ");
-        banner[4] = String.join(" ", "*     *", "*     *", "*      ", "     *");
-        banner[5] = String.join(" ", "*     *", "*     *", "*      ", "*    *");
-        banner[6] = String.join(" ", " ***** ", " ***** ", "*      ", " **** ");
+        // Pattern for letter P
+        CharacterPatternMap P = new CharacterPatternMap('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
 
-        for (String line : banner) {
-            System.out.println(line);
+        // Pattern for letter S
+        CharacterPatternMap S = new CharacterPatternMap('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        // Word OOPS (array of objects)
+        CharacterPatternMap[] word = {O, O, P, S};
+
+        // Print banner
+        for (int i = 0; i < 7; i++) {
+            System.out.println(buildLine(word, i));
         }
     }
 }
