07e2be9ded9a011f99daadb29d3cdce483f5a54f
write2rohanp007-spec <write2rohanp007@gmail.com>
2026-03-09 18:12:47 +0530

    UC4: Render OOPS banner using String array and loop

--- BannerApp.java
+++ BannerApp.java
@@ -2,18 +2,16 @@ public class BannerApp {
 
     public static void main(String[] args) {
 
-        // Array to store all banner lines
-        String[] banner = {
-            String.join(" ", "  *****  ", "  *****  ", " ******  ", " *****  "),
-            String.join(" ", " *     * ", " *     * ", " *     * ", "*     * "),
-            String.join(" ", "*       *", "*       *", " *     * ", "*      "),
-            String.join(" ", "*       *", "*       *", " ******  ", " ***** "),
-            String.join(" ", "*       *", "*       *", " *       ", "      *"),
-            String.join(" ", " *     * ", " *     * ", " *       ", "*     *"),
-            String.join(" ", "  *****  ", "  *****  ", " *       ", " ***** ")
-        };
+        String[] banner = new String[7];
 
-        // Loop to print each line
+        banner[0] = String.join(" ", " ***** ", " ***** ", " ****** ", " ***** ");
+        banner[1] = String.join(" ", "*     *", "*     *", "*     * ", "*     *");
+        banner[2] = String.join(" ", "*     *", "*     *", "*     * ", "*     ");
+        banner[3] = String.join(" ", "*     *", "*     *", "****** ", " ***** ");
+        banner[4] = String.join(" ", "*     *", "*     *", "*      ", "     *");
+        banner[5] = String.join(" ", "*     *", "*     *", "*      ", "*    *");
+        banner[6] = String.join(" ", " ***** ", " ***** ", "*      ", " **** ");
+
         for (String line : banner) {
             System.out.println(line);
         }
